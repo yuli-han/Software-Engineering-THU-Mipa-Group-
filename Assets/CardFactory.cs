5bfac7ccf9ae18f17427282a85b4e083d2082689
郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>
2016-12-02 19:19:09 +0800

    Signed-off-by: 郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>

--- Assets/CardFactory.cs
+++ Assets/CardFactory.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CardFactory {
 
-	static void CreateNewCard(int cost,int atk,int hp,String name,String description,String pictureLocation)
+	static void CreateNewCard(int cost,int atk,int hp,string name,string description,string pictureLocation)
 	{
 		
 	}
