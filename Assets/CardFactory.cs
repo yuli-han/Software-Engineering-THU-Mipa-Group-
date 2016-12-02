7e41c49a89b1373fe799aa51e39ec7339cb5188e
unknown <>
2016-12-02 19:53:34 +0800

    Signed-off-by: unknown <颜晴>

--- Assets/CardFactory.cs
+++ Assets/CardFactory.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class CardFactory {
 
-	static void CreateNewCard(int cost,int atk,int hp,String name,String description,String pictureLocation)
-	{
+	//static void CreateNewCard(int cost,int atk,int hp,String name,String description,String pictureLocation)
+	//{
 		
-	}
+	//}
 }
