1e9c9a53b13b800f321e38cf62c79e3deb11d185
郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>
2016-12-02 21:22:13 +0800

    Signed-off-by: 郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>

--- Assets/Common_NowCardSet.cs
+++ Assets/Common_NowCardSet.cs
@@ -2,23 +2,40 @@
 using System.Collections;
 
 using System.IO;
+
 //NowCardSet:是类型间交流卡组数据的公有类。直接采用静态类。
 
-public class NowCardSet {
+public class Common_NowCardSet {
     static string FileName;
     static public void LoadCardFile(string inputFile)
     {
         FileName = inputFile;
-        FileStream fs = new FileStream(FileName, FileMode.Create);
-        StreamWriter sw = new StreamWriter(fs);
+        StreamReader sr = new StreamReader(FileName, System.Text.Encoding.Default);
+        string line;
+        Length = 0;
+        while ((line = sr.ReadLine()) != null)
+        {
+            CardSet[Length] = int.Parse(line);
+            Length++;
+        }
 
-        sw.Close();
-        fs.Close();
     }
     static public void SaveCardFile()//不需要名字，自动利用即可读取的文件名即可。
     {
-
+        FileStream fs = new FileStream(FileName, FileMode.Create);
+        StreamWriter sw = new StreamWriter(fs);
+        //开始写入
+        for (int i = 0; i < Length; i++)
+        {
+            sw.Write(CardSet[i]+'\n');
+        }
+        //清空缓冲区
+        sw.Flush();
+        //关闭流
+        sw.Close();
+        fs.Close();
     }
-    static int[] CardSet;
+    static int[] CardSet;//卡组
+    static int Length;//卡组长度
 }
 
