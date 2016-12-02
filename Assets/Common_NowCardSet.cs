1904616bfa7ef9ef634c359b2f1154d3e3e66613
郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>
2016-12-02 19:48:16 +0800

    修改了脚本名字以符合区分不同Scene的脚本 Signed-off-by: 郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>

---
+++ Assets/Common_NowCardSet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.IO;
+//NowCardSet:是类型间交流卡组数据的公有类。直接采用静态类。
+
+public class NowCardSet {
+    static string FileName;
+    static public void LoadCardFile(string inputFile)
+    {
+        FileName = inputFile;
+    }
+    static public void SaveCardFile()//不需要名字，自动利用即可读取的文件名即可。
+    {
+
+    }
+    static int[] CardSet;
+}
+
