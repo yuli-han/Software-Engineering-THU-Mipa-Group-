c8ba2bee9bd2366465ae159438083fe6e78069a1
郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>
2016-12-03 13:51:21 +0800

    Signed-off-by: 郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>

---
+++ Assets/GamePlayScene_GameCenterScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//GamePlayScene核心的代码。嘛，也就是整体控制器
+
+public class GamePlayScene_GameCenterScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    //初始化第一步：根据已有的数据来编造卡组
+        //数据来源：Common_NowCardSet.CardSet&.Length
+        
+        //生成的卡片按顺序铺在场上
+
+        //初始化第二步：根据英雄信息，将头像和英雄技能按钮置于场上。
+
+        //初始化第三步：初始化各个控件的信息
+
+        //初始化第四步：设置状态为初始状态。
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
