0197e126120ef771fb998a2dbcc40fe2bb549853
unknown <>
2016-12-03 14:53:18 +0800

    Signed-off-by: unknown <颜晴>

---
+++ Assets/Common_DataBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//此类的用途是用于获取卡片详细数据
+
+public class Common_DataBase 
+{
+
+    static public void GetCardInfo(int cardId)
+    {
+
+    }
+}
--- Assets/GamePlayScene_CardFactory.cs
+++ Assets/GamePlayScene_CardFactory.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class GamePlayScene_CardFactory {
+public class CardFactory :MonoBehaviour{
 
-	static void CreateNewCard(int cost,int atk,int hp,string name,string description,string pictureLocation)
+	public GameObject Card;
+	public Sprite[] picture;
+	
+	public GameObject CreateNewCard(int cost,int atk,int hp,string name,string description,int pictureIndex)
 	{
+		GameObject NewCard = Instantiate(Card);
+		NewCard.transform.FindChild("Card Name").GetComponent<Text>().text=name;
+		NewCard.transform.Find("Description").GetComponent<Text>().text=description;
+		NewCard.transform.Find("Cost").GetComponent<Text>().text=cost.ToString();
+		NewCard.transform.Find("Attack").GetComponent<Text>().text=atk.ToString();
+		NewCard.transform.Find("Life").GetComponent<Text>().text=hp.ToString();
+		NewCard.transform.Find("photo").GetComponent<Image>().sprite = picture[pictureIndex];
 		
+		return(NewCard);
 	}
 }
