f88b4710b18f5e49c2893b7c97f8b27acc6e0c6c
郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>
2016-12-03 15:39:24 +0800

    Signed-off-by: 郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>

--- Assets/Common_CardInfo.cs
+++ Assets/Common_CardInfo.cs
@@ -5,6 +5,19 @@ public class Common_CardInfo : MonoBehaviour {
     
     //卡片数据暂存器。将这个Script附着在卡片上面作为卡片数据，以方便计算以及显示。
 
+    //基本数据
+    string name;
+    string description;
+    int cost;
+    int atk;
+    int hp;
+
+    //额外数据
+    int id;
+    int itemid;
+    int position;
+    //void* Trigger;
+
 	// Use this for initialization
 	void Start () {
 	
