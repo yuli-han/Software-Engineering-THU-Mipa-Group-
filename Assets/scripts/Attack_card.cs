6486edf017bccae76b1584b20b5dacd575cef284
unknown <>
2016-12-09 21:06:37 +0800

    Signed-off-by: unknown <颜晴>

--- Assets/scripts/Attack_card.cs
+++ Assets/scripts/Attack_card.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.EventSystems;
 
@@ -17,6 +18,27 @@ public class Attack_card : MonoBehaviour,IDropHandler{
 			//trigger whatever you want, activator: d; accepter: this.
 			//like: d attack this   ----- attack(d,this);
 			Debug.Log("Card " + d.GetComponent<Common_CardInfo>().cardInfo.name + " attacks " + "card " + this.GetComponent<Common_CardInfo>().cardInfo.name);
+			OnAttackEvent(d,this);
 		}
+	}
+	
+	public void OnAttackEvent(GameObject user,GameObject target)
+	{
+	//血量结算
+	user.GetComponent<Common_CardInfo>().cardInfo.hp-=target.GetComponent<Common_CardInfo>().cardInfo.atk;
+	target.GetComponent<Common_CardInfo>().cardInfo.hp-=user.GetComponent<Common_CardInfo>().cardInfo.atk;
+	
+	//判定自身是否死亡
+	if(user.GetComponent<Common_CardInfo>().cardInfo.hp<=0)
+	{
+		Destroy(user);//user.destroy;
+	}
+	
+	//判定对手是否死亡
+	if(target.GetComponent<Common_CardInfo>().cardInfo.hp<=0)
+	{
+		Destory(target);//target.destroy;
+	}
+	
 	}
 }
