fcef337982e81c343ae25ec6d619947ecc04fc8e
郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>
2016-12-22 19:04:22 +0800

    网络连接部分深化；这次可以接近实用了
    Signed-off-by: 郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>

--- Assets/NetMessage.cs
+++ Assets/NetMessage.cs
@@ -6,15 +6,46 @@ using System.Collections;
 
 public class NetMessage
 {
-	public interface MSGPack
+	public int infoType;
+	public int addint1;//两个附加int，用于额外信息。一般情况下，会对应
+	public int addint2;
+	public static readonly int Attack=1;
+	public static readonly int DrawCard=2;
+	public static readonly int Summon=3;
+	public static readonly int SpellCard=4;
+	public static readonly int TurnChange=5;
+	public static readonly int TriggerExec=6;
+
+	public static NetMessage toMSG(int input,Trigger.TriggerInput inputTrigger)
 	{
-        //int unitId;
-	    string sendMSG();//将类转换为MSG并予以传输
-		void recvMSG(string input);//将MSG转换回类
+		NetMessage output=new NetMessage();
+		output.infoType=input;
+		if(inputTrigger==null)
+		{
+			output.addint1=0;
+            output.addint2 = 0;
+		}
+		else
+		{
+			if(inputTrigger.CardUser==null)
+                output.addint1 = 0;
+            else output.addint1 = inputTrigger.CardUser.GetComponent<Common_CardInfo>().cardInfo.itemid;
+			if(inputTrigger.CardTarget==null)
+                output.addint2 = 0;
+            else output.addint2 = inputTrigger.CardTarget.GetComponent<Common_CardInfo>().cardInfo.itemid;
+
+		}
 	}
-	
-	static int Attack=1;
-	static int DrawCard=2;
-	static int Summon=3;
-	static int SpellCard=4;
+
+    public static NetMessage toMSG(string input)
+    {
+        return null;
+    }
+
+	public override string ToString()
+	{
+        return null;
+	}
+		
+
 }
