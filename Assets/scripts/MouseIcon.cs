0fb46390f206fce3d0430aefb914a18a23da1d02
unknown <>
2016-12-26 18:46:06 +0800

    加入鼠标的贴图 Signed-off-by: unknown <颜晴>

---
+++ Assets/scripts/MouseIcon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseIcon : MonoBehaviour {
+	public Texture2D cursorTexture; 
+	public Texture2D DefaultCursor;
+	private CursorMode cursorMode = CursorMode.Auto;  
+	private Vector2 hotSpot = Vector2.zero;  
+	void OnMouseDown()   
+	{  
+		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+		Debug.Log("鼠标已按下");
+	}  
+	void OnMouseUp()
+	{
+		Cursor.SetCursor(DefaultCursor, hotSpot, cursorMode);
+	}
+}
