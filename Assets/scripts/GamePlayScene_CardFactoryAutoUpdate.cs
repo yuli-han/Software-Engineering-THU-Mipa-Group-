7440af8968ac77f0a8dbafc38342c6fe167483e4
郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>
2016-12-30 18:41:43 +0800

    捂脸用 Signed-off-by: 郑嘉诺 <zhengjn13@mails.tsinghua.edu.cn>

---
+++ Assets/scripts/GamePlayScene_CardFactoryAutoUpdate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GamePlayScene_CardFactory))]
+[RequireComponent(typeof(CardSetScene_CardFactory))]
+public class GamePlayScene_CardFactoryAutoUpdate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<GamePlayScene_CardFactory>().frame = GetComponent<CardSetScene_CardFactory>().frame;
+        GetComponent<GamePlayScene_CardFactory>().picture = GetComponent<CardSetScene_CardFactory>().picture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
